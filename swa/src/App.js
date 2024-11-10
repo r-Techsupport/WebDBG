9f304c0a5be1c531b6d3205e4799d78310f8c97a
PipeItToDevNull <piper@dev0.sh>
2024-11-09 22:05:51 -0500

    finalize website rendering method
    Render only human readable dmpInfo and analysis with rawContent being hidden under a summary. The other fields will only be used for machine post-processing on the API side and never served to the user.

--- swa/src/App.js
+++ swa/src/App.js
@@ -99,80 +99,64 @@ const FileUpload = () => {
     
     // Function to render JSON object into HTML objects
     const renderJsonToHtml = (data) => {
+        console.log("Data received:", data); // Debugging line
+
+        // No clue what this does, but everything is rendered inside it
         if (Array.isArray(data)) {
             return data.map((item, index) => (
-                <div key={index} style={{ marginLeft: '20px' }}>
-                {renderJsonToHtml(item)}
-                </div>
-            ));
-        } else if (typeof data === 'object' && data !== null) {
-            // Define the key order to display in
-            const order = [
-                "dmpInfo",
-                "analysis",
-                "rawContent",
-                "BUGCHECK_CODE",
-                "BUGCHECK_P1",
-                "BUGCHECK_P2",
-                "BUGCHECK_P3",
-                "BUGCHECK_P4",
-                "FILE_IN_CAB",
-                "BUGCHECK_CODE",
-                "BUGCHECK_P1",
-                "BUGCHECK_P2",
-                "BUGCHECK_P3",
-                "BUGCHECK_P4",
-                "FILE_IN_CAB",
-                "SECURITY_COOKIE",
-                "BLACKBOXBSD",
-                "BLACKBOXNTFS",
-                "BLACKBOXPNP",
-                "BLACKBOXWINLOGON",
-                "PROCESS_NAME"
-            ];
-            const specialKeys = ["dmpInfo", "analysis", "rawContent"];
-            const sortedData = sortJson(data, order);
-            
-            // Convert object to array of key-value pairs
-            const keyValueArray = Object.entries(sortedData).map(([key, value]) => ({ key, value }));
-            
-            // Separate the special items
-            const specialItems = keyValueArray.filter(item => specialKeys.includes(item.key));
-            const regularItems = keyValueArray.filter(item => !specialKeys.includes(item.key));
-            
-            // Render the regular items
-            const regularRender = regularItems.map((item, index) => (
                 <>
-                <dt>{item.key}</dt>
-                <dd>{item.value}</dd>
+                    {renderJsonToHtml(item)}
                 </>
             ));
-            
-            // Render the special items with their own method
-            const specialRender = specialItems.map((item, index) => (
-                <div key={index} className={item.key}>
-                <details>
-                    <summary>{item.key}</summary>
-                    <pre>{item.value}</pre>
-                </details>
-                </div>
-            ));
-            
-            // Combine both renders
-            return (
-                <>
-                    <dl>
-                        {regularRender}
-                    </dl>
-                    <div>
-                        {specialRender}
-                    </div>
-                </>
-            );
-            
         }
-        return {data};
+
+        // Define the key order to display in
+        const order = [
+            "dmpInfo",
+            "analysis",
+            "rawContent",
+        ];
+        const specialKeys = ["rawContent"];
+        const sortedData = sortJson(data, order);
+        
+        // Convert object to array of key-value pairs
+        const keyValueArray = Object.entries(sortedData).map(([key, value]) => ({ key, value }));
+        
+        // Separate the special items
+        const specialItems = keyValueArray.filter(item => specialKeys.includes(item.key));
+        const regularItems = keyValueArray.filter(item => !specialKeys.includes(item.key));
+        
+        // Render the regular items
+        const regularRender = regularItems.map((item, index) => (
+            <>
+            <h2 class={`${item.key} result-header`}>
+                {item.key}
+            </h2>
+            <div class="result-content">
+                {item.value}
+            </div>
+            </>
+        ));
+        
+        // Render the special items with their own method
+        const specialRender = specialItems.map((item, index) => (
+            <div key={index} className={item.key}>
+            <details>
+                <summary>Raw results</summary>
+                <div class="result-content">{item.value}</div>
+            </details>
+            </div>
+        ));
+        
+        // Combine both renders
+        return (
+            <>
+            {regularRender}
+            {specialRender}
+            </>
+        );
     };
+
     
     
     
@@ -184,25 +168,25 @@ const FileUpload = () => {
         </Helmet>
             <div id="container"> 
                 <div id="header">
-                <h1 id="site_name">{SITE_NAME}</h1>
+                    <h1 id="site_name">{SITE_NAME}</h1>
                 </div>
                 <div class="button-container">
                     <div class="button-div">
-                    <input type="file" onChange={handleFileChange} />
-                    <button onClick={handleFileUpload} disabled={loading}>
-                    {loading ? 'Uploading...' : 'Upload File'}
-                    </button>
+                        <input type="file" onChange={handleFileChange} />
+                        <button onClick={handleFileUpload} disabled={loading}>
+                            {loading ? 'Uploading...' : 'Upload File'}
+                        </button>
                     </div>
                     <div class="button-div">
-                    <input type="text" value={url} onChange={handleUrlChange} placeholder="Enter URL" />
-                    <button onClick={handleUrlSubmit} disabled={loading}>
-                    {loading ? 'Submitting...' : 'Upload URL'}
-                    </button>
+                        <input type="text" value={url} onChange={handleUrlChange} placeholder="Enter URL" />
+                        <button onClick={handleUrlSubmit} disabled={loading}>
+                            {loading ? 'Submitting...' : 'Upload URL'}
+                        </button>
                     </div>
                 </div>
                 
                 <div id="content">
-                {!error && !responseData && <p>Upload your .dmp file or provide a download link above</p>}
+                {!error && !responseData && <p>{loading ? 'Processing...' : 'Upload your .dmp file or provide a download link above'}</p>}
                 {error && <p style={{ color: 'red' }}>{error}</p>}
                 {responseData && (
                     <>{renderJsonToHtml(JSON.parse(responseData))}</>
